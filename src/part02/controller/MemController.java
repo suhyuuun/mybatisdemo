3b483ca631881d8f8ee9bc00df4b7eb542d61587
suhyun <suhyuuuuuun@gmail.com>
2022-11-04 17:51:38 +0900

    20221104 | update

---
+++ src/part02/controller/MemController.java
@@ -0,0 +1,31 @@
+package part02.controller;
+
+import java.util.HashMap;
+import java.util.List;
+
+import part02.dao.MemDAO;
+import part02.dto.MemDTO;
+
+public class MemController {
+   private MemDAO dao;
+
+   public MemController() {
+      dao = new MemDAO();
+   }
+
+   public List<MemDTO> listProcess() {
+      return dao.listMethod();
+   }
+
+   public int insertProcess(MemDTO dto) {
+      return dao.insertMethod(dto);
+   }
+
+   public int updateProcess(HashMap<String, Object> hmap) {
+      return dao.updateMethod(hmap);
+   }
+
+   public int deletProcess(int age) {
+      return dao.deleteMethod(age);
+   }
+}
